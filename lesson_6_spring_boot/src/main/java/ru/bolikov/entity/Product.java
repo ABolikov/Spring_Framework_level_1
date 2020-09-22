d79a6539706c148d8320d1db55f374dd8ad5daf2
ABolikov <Brolond@mail.ru>
2020-09-23 00:38:56 +0600

    Перенос кода на spring boot.

---
+++ lesson_6_spring_boot/src/main/java/ru/bolikov/entity/Product.java
@@ -0,0 +1,69 @@
+package ru.bolikov.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "cost")
+    private Integer cost;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String title, Integer cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!title.equals(product.title)) return false;
+        return cost.equals(product.cost);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + cost.hashCode();
+        return result;
+    }
+}
---
+++ lesson_6_spring_boot/src/test/java/ru/bolikov/Lesson6SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bolikov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
